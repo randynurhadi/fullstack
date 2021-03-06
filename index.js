1659c6766dc1894e08265b197fb09236f386b986
randynurhadi <nurhadirandy@gmail.com>
2021-03-06 15:23:57 +0700

    add files index

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
-fullstack.js
+// fullstack.js
 
-mencoba menambahkan File
-sukses menambahkan file kedalam git hub
+// mencoba menambahkan File
+// sukses menambahkan file kedalam git hub4
+
+// var num1 = 25;
+// var num2 = "25";
+
+// var total = num1 - num2;
+// console.log(total);
+
+
+// operator logic
+// And, OR, NOT, ==, ===
+
+// var x = 10;
+// var y = '10';
+
+// var nilai = x === y;
+// console.log(nilai);
+
+
+var nilai = 90;
+if (nilai < 100) {
+    console.log("tidak maksimal");
+}
+else if (nilai > 100){
+    console.log("maksimal");
+}
